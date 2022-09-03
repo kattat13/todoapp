b2b099843faed57ae43fd578c502234f0f6ae23f
niekoniecznie <cyberbzdety@gmail.com>
2022-09-03 18:43:58 +0200

    API addition

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'todo',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
